e21b280c7bab2a7f7aef69bc9dcd162da92568b6
witsemp <61912100+witsemp@users.noreply.github.com>
2020-05-31 17:10:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,6 @@ def process_obs(obs):
     # crop the status bar
     obs_thresh = obs_thresh[0:84, 0:96]
 
-    # convert thresholded image back to BGR
-    # obs_thresh = cv.cvtColor(obs_thresh, cv.COLOR_GRAY2RGB)
-
-    # draw starting point
-    # obs_thresh[67:77, 46:50] = (0, 0, 255)
-
     # resize to match game window size
     obs_thresh = cv.resize(obs_thresh, (1000, 700))
 
@@ -62,50 +56,35 @@ def process_obs(obs):
         goal_point = (round(goal_row), round(goal_col))
         # print("Goal point" + str(goal_point))
 
-    # obs_thresh[goal_point[0]:goal_point[0]+10, goal_point[1]:goal_point[1]+10, 0] = 255
-    # obs_thresh[goal_point[0]:goal_point[0]+10, goal_point[1]:goal_point[1]+10, 1] = 0
-    # obs_thresh[goal_point[0]:goal_point[0]+10, goal_point[1]:goal_point[1]+10, 2] = 0
-
-    # goal_point = (goal_point[1], goal_point[0])
     end_point = (goal_point[1], goal_point[0])
     wh_pixels_left.clear()
     wh_pixels_right.clear()
     wh_pixels_up.clear()
-    # cv.imshow('thresh', obs_thresh)
     return obs_thresh, start_point, end_point
 
 def draw_path(points, graph):
     obs_rgb = cv.imread('obs.png', 1)
     obs_rgb = obs_rgb[0:84, 0:96, :]
     obs_rgb = cv.resize(obs_rgb, (1000, 700))
-    # cv.imwrite('rgb_resized.png', obs_rgb)
     for key, value in graph.items():
         obs_rgb[key[1]:key[1]+5, key[0]:key[0]+5, 0] = 0
         obs_rgb[key[1]:key[1]+5, key[0]:key[0]+5, 1] = 0
         obs_rgb[key[1]:key[1]+5, key[0]:key[0]+5, 2] = 255
-    # for point in points:
-    #     obs_rgb[point[1]:point[1]+5, point[0]:point[0]+5, 0] = 255
-    #     obs_rgb[point[1]:point[1]+5, point[0]:point[0]+5, 1] = 0
-    #     obs_rgb[point[1]:point[1]+5, point[0]:point[0]+5, 2] = 0
     a = np.array(points)
-    # cv.drawContours(obs_rgb, [a], 0, (255, 0, 0), 2)
     for point1, point2 in zip(a, a[1:]):
         cv.line(obs_rgb, tuple(point1), tuple(point2), [255, 0, 0], 2)
     cv.imshow('hehe', obs_rgb)
 
-
 env = gym.make('CarRacing-v0')
-observation = env.reset()
-# process_obs(observation)
+env.reset()
 render = 0
 while render < 5000:
     render = render + 1
     env.render()
-    action = [0, 0.01, 0]
+    action = [0, 0.15, 0]
     print("Render no: " + str(render))
-    print("--------------------------------")
     observation, reward, done, info = env.step(action)
-    if render > 50:
+    if render >= 50:
         thresh, start_point, goal_point = process_obs(observation)
         RRT = rrt.RRT(thresh, start_point, goal_point)
         path, graph = RRT.search()
